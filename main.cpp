53d7d0fbb4b7c1f8d3c3afae80077a918af71bd4
Sufyansiddiquix <sufyan.siddiqui7maz@gmail.com>
2024-05-11 21:25:21 +0500

    just committed

--- main.cpp
+++ main.cpp
@@ -331,7 +331,7 @@ void RunGame()
     CloseWindow();
 }
 
-// Function to display the main menu screen ..
+// Function to display the main menu screen 
 void ShowMainMenu()
 {
     const int screenWidth = 800;
