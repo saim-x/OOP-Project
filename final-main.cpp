a0a1d1cf4e8a9392fe4e4741ad2149074b7f6285
Muhammad-TalhaSaquib001 <talhasaquib03@gmail.com>
2024-05-14 00:06:05 +0500

    Sync

--- final-main.cpp
+++ final-main.cpp
@@ -1,1 +1,23 @@
+#include "include/raylib.h"
+#include <cmath>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+class SpaceObjects
+{
+private:
+    // Attributes
+    Texture2D image_;
+    Vector2 position_;
+
+public:
+    // Constructors
+    SpaceObjects(const Texture2D image, const Vector2 position) : image_(image)
+    {
+        }
+};
